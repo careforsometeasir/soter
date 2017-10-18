1c8441392cf0358001797f7b7157ef209a00f42b
careforsometeasir <richywypi@gmail.com>
2017-10-18 19:20:40 +0100

    Make more compact

--- soteruser.py
+++ soteruser.py
@@ -15,45 +15,26 @@ def hashify(password, salt):
     """Hashes the password using SHA512 with the salt"""
     return hashlib.sha512(password.encode('utf-8')+salt).hexdigest()
 
-def newUser(username, password):
+def newUser(username, password, length=16):
     """Adds a user and returns user list"""
     if username in users:
         return 3
     else:
-        salty = salt(16)
+        salty = salt(length)
         hashedPass = hashify(password, salty)
         users[username]={"password":hashedPass,"salt":salty.decode('utf-8')}
         return users
 
 
-def generateUser(username, password):
+def generateUser(username, password, length=16):
     """Generates a user"""
     salty = salt(16)
     hashedPass = hashify(password, salty)
     content = {username:{"password":hashedPass, "salt":salty.decode('utf-8')}}
     return content
 
-def loginLocal(username, password):
-    """Checks password for username using local database"""
-    #parsed = json.loads("{"+content+"}")
-    #print(parsed[username])
-    if username in users:
-        data = users[username]
-        #print(data)
-        retrievedPassword = data["password"]
-        salty = data["salt"].encode('utf-8')
-        newPassword = hashify(password, salty)
-        #print(newPassword)
-        if retrievedPassword == newPassword:
-            #print("Success")
-            return 1
-        else:
-            #print("Access Denied")
-            return 2
-    else:
-        return 4
 
-def login(username, password, content):
+def login(username, password, content=users):
     """Checks password for username in provided content"""
     if username in content:
         data = content[username]
