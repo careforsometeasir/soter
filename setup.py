4c6b2b59ce78b66bca2df9f134366a64984d2a30
Ryszard <richywypi@gmail.com>
2017-10-18 17:22:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+with open(os.path.join(mypackage_root_dir, 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='soter',
-    version='0.1',
+    version=version,
 
     description='A module to store usernames, hashed passwords and salts',
+    long_description=long_description,
     
     url='https://github.com/careforsometeasir/soter/',
     
